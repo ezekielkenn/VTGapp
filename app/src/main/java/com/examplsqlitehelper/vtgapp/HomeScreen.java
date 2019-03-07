f4778dd6808b3d506b77099152050d8ca1e8144c
ezekiel <ezekielvillaester@gmail.com>
2019-03-08 01:43:46 +0800

    Commit

---
+++ app/src/main/java/com/examplsqlitehelper/vtgapp/HomeScreen.java
@@ -0,0 +1,40 @@
+package com.examplsqlitehelper.vtgapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import com.google.firebase.auth.FirebaseAuth;
+public class HomeScreen extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListner;
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListner);
+    }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_homescreen);
+//        Button button = (Button) findViewById(R.id.signout);
+//        mAuth = FirebaseAuth.getInstance();
+//        mAuthListner = new FirebaseAuth.AuthStateListener() {
+//            @Override
+//            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+//                if (firebaseAuth.getCurrentUser()==null)
+//                {
+//                    startActivity(new Intent(Home_screen.this, singin_activity.class));
+//                }
+//            }
+//        };
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mAuth.signOut();
+//            }
+//        });
+    }
